ab7ed389360b06271d60196ab7f3aec8ecf0ae15
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2021-12-11 16:42:50 +0530

    Add files via upload

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import TabNavigator from "./TabNavigator";
+import Profile from "../screens/Profile";
+
